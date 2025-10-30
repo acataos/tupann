788b0a9c21e00eda95987f8dc855ab343a4c76c3
acataos <acatao1999@gmail.com>
2025-10-30 17:30:43 -0300

    Streamline SatelliteData class

--- src/data/process/SatelliteData.py
+++ src/data/process/SatelliteData.py
@@ -1,4 +1,4 @@
-from datetime import datetime, timedelta
+from datetime import datetime
 from pathlib import Path
 
 import numpy as np
@@ -13,31 +13,17 @@ class SatelliteData:
         data: pd.DataFrame,
         product: list[str],
         value: str | None = None,
-        day: str | None = None,
-        folder: str | None = None,
     ) -> None:
         self.data = data
         self.product = product
         self.value = value
-        self.day = datetime.strptime(day, "%Y-%m-%d").date()
-        self.folder = folder
 
     @classmethod
-    def load_data(cls, input_filepath: Path | str, value: str | None = None):
+    def load_data(cls, input_filepath: Path | str, product: str | None = None, value: str | None = None):
         return cls(
             pd.read_feather(input_filepath),
-            Path(input_filepath).parent.name,
+            product,
             value,
-            Path(input_filepath).stem,
-            Path(input_filepath).parents[1],
-        )
-
-    def _load_previous_day(self):
-        self.data = pd.concat(
-            [
-                pd.read_feather(f"{self.folder}/{self.product}/{self.day - timedelta(days=1)}.feather"),
-                self.data,
-            ]
         )
 
     def cast_to_float32(self):
@@ -46,9 +32,10 @@ class SatelliteData:
                 self.data[col] = self.data[col].astype(np.float32)
 
     def interp_at_grid(self, band: str, timestamp: datetime, target_grid: NDArray):
-        self._load_previous_day()
-        assert (timestamp >= self.data["creation"]).any(), "Timestamp passed precedes all timestamps in the data"
-        closest_timestamp = self.data.loc[self.data["creation"] <= timestamp, "creation"].max()
+        assert (timestamp >= self.data["creation"]).any(
+        ), "Timestamp passed precedes all timestamps in the data"
+        closest_timestamp = self.data.loc[self.data["creation"]
+                                          <= timestamp, "creation"].max()
         df = self.data[self.data["creation"] == closest_timestamp]
         column = f"{self.value}_{band}" if self.product == "ABI-L2-MCMIPF" else self.value
 
@@ -65,7 +52,8 @@ class SatelliteData:
             interp_values = interpolate.griddata(
                 points,
                 values,
-                (target_grid[:, :, 1].flatten(), target_grid[:, :, 0].flatten()),
+                (target_grid[:, :, 1].flatten(),
+                 target_grid[:, :, 0].flatten()),
                 method="linear",
             ).reshape(shape)
         except ValueError:
